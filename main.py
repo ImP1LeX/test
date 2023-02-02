305d188e64fcc1a9c776814f33355219e4561a3c
ImP1LeX <artem04112004@icloud.com>
2023-02-02 17:24:34 +0500

    test

--- main.py
+++ main.py
@@ -1,15 +1,44 @@
-# This is a sample Python script.
+import aiohttp
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import logging
+from aiogram import Bot, Dispatcher, executor, types
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+
+DOLLAR = 70.07
+EURO = 77.02
+
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token="5479233793:AAH9JgA6kuSIT9q_4v_K1Gl4BdD3E2k_IBs")
+dp = Dispatcher(bot=bot)
+
+@dp.message_handler(commands=["start"])
+async def cmd_start(message: types.Message):
+    kb = [
+        [
+        types.KeyboardButton(text="Доллары"),
+        types.KeyboardButton(text="Евро")
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True
+    )
+    await message.answer("Выберите валюту", reply_markup=keyboard)
+
+
+@dp.message_handler(text=["Доллары"])
+async def dollar_cmd(message: types.Message):
+    await message.answer(f"Цена: {DOLLAR}")
+
+@dp.message_handler(text=["Евро"])
+async def euro_cmd(message: types.Message):
+    await message.answer(f"Цена: {EURO}")
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
+
+
 
